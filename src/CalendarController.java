5062a5f57d872f117d429a8262a9d25766b2d9c2
mike123789-dev <mike123789@g.skku.edu>
2021-05-16 14:45:11 +0900

    add comments

--- src/CalendarController.java
+++ src/CalendarController.java
@@ -1,4 +1,4 @@
-
+//앱 내 달력/일정과 관련된 비즈니스 로직을 담당하는 Controller 
 public class CalendarController {
 	private CalendarPane calendarPane;
 	
@@ -7,6 +7,7 @@ public class CalendarController {
 	}
 	
 	public void addNewScheudle() {
+		//임시로 text만 바꾸게
 		this.calendarPane.calendarLabel.setText("새로운 일정 추가!");
 	}
 	
--- src/CalendarPane.java
+++ src/CalendarPane.java
@@ -5,11 +5,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CalendarPane extends JPanel {
+	//TODO: 달력 화면 보여주기
 	JLabel calendarLabel;
+	
 	CalendarPane() {
 		calendarLabel = new JLabel("calendar");
 		add(calendarLabel);
-//		this.setPreferredSize(new Dimension(400,100));
 		setBackground(Color.green);
 	}
 }
